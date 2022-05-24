54a9aff2335df6b089624fb3ff768d9dab244f42
hasanTheBest <rainbowmahmud@gmail.com>
2022-05-25 03:31:42 +0600

    Manage user added.

--- src/pages/Protected/Admin/ManageOrders.jsx
+++ src/pages/Protected/Admin/ManageOrders.jsx
@@ -1,7 +1,51 @@
+import axios from "axios";
 import React from "react";
+import { useQuery } from "react-query";
+import ManageOrderRow from "./ManageOrders/ManageOrderRow";
+import Loading from "../../../Components/Loading";
+import Error from "../../../Components/Error";
 
 const ManageOrders = () => {
-  return <div>ManageOrders</div>;
+  const {
+    isLoading,
+    isError,
+    refetch,
+    error: queryErr,
+    data: queryData,
+  } = useQuery("ordersByUsers", () => axios.get("user/purchases"));
+
+  // Query state
+  if (isError) {
+    return <Error msg={queryErr?.message} />;
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <section>
+        <div class="overflow-x-auto w-full">
+          <table class="table w-full">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Client</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {queryData.data.map((item) => (
+                <ManageOrderRow key={item._id} {...item} refetch={refetch} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ManageOrders;
---
+++ src/pages/Protected/Admin/ManageOrders/ManageOrderRow.jsx
@@ -0,0 +1,87 @@
+import axios from "axios";
+import React from "react";
+import toast from "react-hot-toast";
+import { useMutation } from "react-query";
+
+const ManageOrderRow = ({
+  _id: orderId,
+  name: buyerName,
+  email: buyerEmail,
+  quantity,
+  purchasedItem,
+  refetch,
+}) => {
+  const { name, image, price } = purchasedItem;
+
+  const {
+    isError,
+    isSuccess,
+    isLoading,
+    mutate,
+    error: queryErr,
+    data: queryData,
+  } = useMutation((payload) => {
+    return axios.delete("/user/purchases/" + orderId, payload);
+  });
+
+  // React query state
+  if (isError) {
+    toast.error(queryErr.message);
+  }
+
+  if (isSuccess) {
+    const { data } = queryData;
+    if (data.deletedCount > 0) {
+      toast.success("Successfully removed a order");
+      refetch();
+    }
+  }
+
+  const handleClickShipOrder = () => {
+    console.log("handleClickShipOrder");
+    // ref.current.addEventListener("click", (e) => {
+    //   e.stopImmediatePropagation();
+    //   // mutate({});
+    // });
+  };
+
+  return (
+    <tr>
+      <td>
+        <div class="flex items-center space-x-3">
+          <div class="avatar">
+            <div class="mask mask-squircle w-16 h-16">
+              <img src={image} alt={name} title={name} />
+            </div>
+          </div>
+          <div>
+            <div class="font-bold" title={name}>
+              {name.length > 20 ? name.slice(0, 20) + "..." : name}
+            </div>
+            <div class="text-sm opacity-50">
+              Ordered: {quantity}pcs, Unit price: {price}
+            </div>
+          </div>
+        </div>
+      </td>
+      <td>
+        {buyerName}
+        <br />
+        <span class="badge badge-ghost badge-sm">{buyerEmail}</span>
+      </td>
+      <td>
+        <button class="btn btn-info btn-outline btn-xs">unpaid</button>
+      </td>
+      <th>
+        <button
+          class={`btn btn-secondary btn-xs ${isLoading ? "loading" : ""}`}
+          onClick={handleClickShipOrder}
+        >
+          Ship
+        </button>
+      </th>
+    </tr>
+  );
+};
+
+export default ManageOrderRow;
